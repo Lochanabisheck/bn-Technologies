2dfe8effbf40c31964251a7ab1fa01436529fc7e
Lochan Abisheck <101311309+Lochanabisheck@users.noreply.github.com>
2026-07-21 08:44:41 +0530

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
 
-$bookingRecipient = 'appointments@bntechnologies.ai';
-$bookingSender = 'website@bntechnologies.ai';
+$bookingRecipient = 'lochanabisheck@gmail.com';
+$bookingSender = 'programmingwithla1@gmail.com';
 $siteName = 'BN Technologies';
